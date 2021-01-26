039b387a6140a682aff0dedbf00e65baf42e8678
Mishal Rahman <mishalbishal@gmail.com>
2021-01-26 09:58:31 -0500

    test staff user for moderation

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,5 +5,6 @@ set -e
 python manage.py makemigrations 2>&1
 python manage.py migrate 2>&1
 python manage.py loaddata fixtures/sites.json
+python manage.py loaddata fixtures/auth.json
 
 python manage.py runserver 0.0.0.0:8000
